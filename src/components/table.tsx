3daf6a9e10013edee88e5b256a12730dd2cda69a
Alireza <alireza.nasseh@gmail.com>
2021-08-21 13:21:48 +0430

    Completed table interface

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,9 +1,84 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {createUseStyles} from 'react-jss';
+import {MainContext} from '../context/mainProvider';
 
 export default function Table() {
+    const classes = useStyles();
+    const {data, addItem, editItem, removeItem} = useContext(MainContext);
+
     return (
         <div>
-            Table
+            <button className={classes.button}>Add</button>
+            <div className={classes.tableContainer}>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Label</th>
+                        <th>Vision</th>
+                        <th>Ability</th>
+                        <th>Delete</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data && data.map((item, index) =>
+                        <tr>
+                            <td>
+                                <input type={'text'} value={item.label}/>
+                            </td>
+                            <td>
+                                <input type={'number'} value={item.vision}/>
+                            </td>
+                            <td>
+                                <input type={'number'} value={item.ability}/>
+                            </td>
+                            <td>
+                                <button className={classes.button}>Delete</button>
+                            </td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
+
+const useStyles = createUseStyles({
+    button: {
+        backgroundColor: '#dfdfe5',
+        border: '1px solid #bbbbc0',
+        borderRadius: '5px',
+        padding: '3px 10px'
+    },
+    tableContainer: {
+        marginTop: '5px',
+        '& table': {
+            width: '100%',
+        },
+        '& th': {
+            backgroundColor: '#a4b1bc',
+            borderRadius: '5px',
+            color: 'white',
+            fontWeight: 'normal',
+            '&:first-child': {
+                width: '40%'
+            }
+        },
+        '& input': {
+            width: 'calc(100% - 10px)',
+            border: '1px solid #bbbbc0',
+            borderRadius: '5px',
+            boxSizing: 'border-boxing',
+            padding: '5px',
+
+            '&:focus': {
+                outline: '0px',
+                border: '1px solid #264073',
+                boxShadow: 'inset 0 0 2px #264073'
+            }
+        },
+        '& button': {
+            width: '100%'
+        }
+    },
+});
