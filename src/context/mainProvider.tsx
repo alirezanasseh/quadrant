3232f2e81667762333eeb78c74a3be8e53f5b9d5
Alireza <alireza.nasseh@gmail.com>
2021-08-21 13:21:17 +0430

    Added main provider for operations

---
+++ src/context/mainProvider.tsx
@@ -0,0 +1,100 @@
+import React, {createContext, useState} from 'react';
+import {IMainProvider} from '../types/mainProvider';
+import {IData, IDataItem} from '../types/data';
+
+export const MainContext = createContext<IMainProvider>({});
+
+const MainProvider = (props: React.PropsWithChildren<{}>) => {
+    // Checking first time load
+    const visitedBefore = localStorage.getItem('visitedBefore');
+    if(!visitedBefore){
+        // Initialize data
+        const initialData: IData = [
+            {
+                label: '1',
+                vision: 10,
+                ability: 10
+            },
+            {
+                label: '2',
+                vision: 40,
+                ability: 40
+            },
+            {
+                label: '3',
+                vision: 80,
+                ability: 30
+            }
+        ];
+        localStorage.setItem('data', JSON.stringify(initialData));
+
+        // Set visited before
+        localStorage.setItem('visitedBefore', 'true');
+    }
+
+    // Getting data from local storage
+    const savedData = localStorage.getItem('data');
+    const dataObj = savedData ? JSON.parse(savedData) : [];
+
+    // Adding a new item to the list
+    const addItem = (item: IDataItem) => {
+        // Add to the state
+        let newData = state.data ? [...state.data] : [];
+        newData.push(item);
+        setState({...state, data: newData});
+
+        // Update local storage data
+        updateLocalStorageData(newData);
+    }
+
+    // Removing an item from the list by index
+    const removeItem = (index: number) => {
+        if(!state.data || state.data.length === 0){
+            return;
+        }
+
+        // Removing from state
+        let newData = [...state.data];
+        newData.splice(index, 1);
+        setState({...state, data: newData});
+
+        // Update local storage data
+        updateLocalStorageData(newData);
+    };
+
+    const editItem = (index: number, item: IDataItem) => {
+        if(!state.data || state.data.length === 0){
+            return;
+        }
+
+        // Updating state
+        let newData = [...state.data];
+        newData[index] = item;
+        setState({...state, data: newData});
+
+        // Update local storage data
+        updateLocalStorageData(newData);
+    };
+
+    const updateLocalStorageData = (newData: IData) => {
+        if(!newData){
+            newData = [];
+        }
+        localStorage.setItem('data', JSON.stringify(newData));
+    };
+
+    const [state, setState] = useState<IMainProvider>({
+        data: dataObj,
+        addItem,
+        removeItem,
+        editItem
+    });
+
+    return (
+        <MainContext.Provider value={state}>
+            {props.children}
+        </MainContext.Provider>
+    );
+}
+
+export default MainProvider;
