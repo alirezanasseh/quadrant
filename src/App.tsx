924750055b9aa44a5367cdc0ae53dff3c8f185eb
Alireza <alireza.nasseh@gmail.com>
2021-08-21 20:30:51 +0430

    Adding data and operation methods to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,99 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from './layout/layout';
 import {Chart, Table} from './components';
 import {createUseStyles} from 'react-jss';
+import {IData, IDataItem, IField} from './types/data';
+import {initialData} from './helper/faker';
 
 function App() {
     const classes = useStyles();
+    const [data, setData] = useState<IData>();
+    const [visitedBefore, setVisitedBefore] = useState(localStorage.getItem('visitedBefore') ?? '');
+
+    useEffect(() => {
+        if(!visitedBefore){
+            localStorage.setItem('data', JSON.stringify(initialData));
+
+            // Set visited before
+            localStorage.setItem('visitedBefore', 'true');
+            setVisitedBefore('true');
+        }
+
+        // Getting data from local storage
+        const savedData = localStorage.getItem('data');
+        setData(savedData ? JSON.parse(savedData) : []);
+    }, []);
+
+    // Adding a new item to the list
+    const addItem = (item: IDataItem) => {
+        // Add to the state
+        let newData = data ? [...data] : [];
+        newData.push(item);
+        setData(newData);
+
+        // Update local storage data
+        updateLocalStorageData(newData);
+    }
+
+    // Removing an item from the list by index
+    const removeItem = (index: number) => {
+        if(!data || data.length === 0){
+            return;
+        }
+
+        // Removing from state
+        let newData = [...data];
+        newData.splice(index, 1);
+        setData(newData);
+
+        // Update local storage data
+        updateLocalStorageData(newData);
+    };
+
+    // Editing an item in the list
+    const editItem = (index: number, field: IField, value: string | number) => {
+        if(!data || data.length === 0){
+            return;
+        }
+
+        // Checking number fields
+        if((field === 'vision' || field === 'ability') && value > 100){
+            value = 100;
+        }
+
+        // Updating state
+        let newData = [...data];
+        newData[index] = {...newData[index], [field]: value};
+
+        setData(newData);
+
+        // Update local storage data
+        updateLocalStorageData(newData);
+    };
+
+    const updateLocalStorageData = (newData: IData) => {
+        if(!newData){
+            newData = [];
+        }
+        localStorage.setItem('data', JSON.stringify(newData));
+    };
 
     return (
         <Layout title={'Home'}>
             <div className={classes.row}>
                 <div className={classes.col}>
-                    <Chart/>
+                    <Chart
+                        data={data}
+                        editItem={editItem}
+                    />
                 </div>
                 <div className={classes.col}>
-                    <Table/>
+                    <Table
+                        data={data}
+                        addItem={addItem}
+                        editItem={editItem}
+                        removeItem={removeItem}
+                    />
                 </div>
             </div>
         </Layout>
