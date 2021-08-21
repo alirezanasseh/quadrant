d8c8161587ae111cef0313383c6917aa29832c10
Alireza <alireza.nasseh@gmail.com>
2021-08-21 10:45:43 +0430

    Placed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import Layout from './layout/layout';
+import {Chart, Table} from './components';
+import {createUseStyles} from 'react-jss';
+
+const useStyles = createUseStyles({
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    col: {
+        width: '100%'
+    }
+});
 
 function App() {
+    const classes = useStyles();
+
     return (
         <Layout title={'Home'}>
-            Hi
+            <div className={classes.row}>
+                <div className={classes.col}>
+                    <Chart/>
+                </div>
+                <div className={classes.col}>
+                    <Table/>
+                </div>
+            </div>
         </Layout>
     );
 }
